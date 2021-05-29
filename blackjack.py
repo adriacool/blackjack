bbfdbf062dd6cb5cc318428bef7d4198ff6f4fb3
Medium Size <adriafrance@my.unt.edu>
2021-05-28 20:30:49 -0500

    Make function calc_score

--- blackjack.py
+++ blackjack.py
@@ -17,11 +17,29 @@ def deal_hand(deck):
       hand.append(deck.pop(randint(0, len(deck)-1)))
     return hand
 
+def calc_score(hand):
+    score = 0
+    aces = 0
+    for i in range(len(hand)):
+        if type(hand[i][1]) == int:
+            score += hand[i][1]
+        elif hand[i][1] != "Ace":
+            score += 10
+        else:
+            aces += 1
+    if aces != 0:
+        for i in range(aces):
+            if score <= 10:
+                score += 11
+            else:
+                score += 1    
+    return(score)      
+
 
 deck = make_deck(suits, nums)
 dealer_hand = deal_hand(deck)
 player_hand = deal_hand(deck)
 print("Dealer showing: ",dealer_hand[1])
 print("Your hand: ", player_hand)
-
+print(calc_score(player_hand))
 
